e4718516851b4e6b1c3fce3fc158cf72a3aa09f3
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-07-12 17:17:09 +0200

    Add flag to load from YAML file

--- urlshortener/app/main.go
+++ urlshortener/app/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/JordiGilTomas/coursecalhoun/urlshortener/urlshort"
 )
 
+var yamlFile string
+
+func init() {
+	flag.StringVar(&yamlFile, "yaml", "routes.yaml", "Carga un fichero YAML")
+}
 func main() {
+	flag.Parse()
+	file, err := os.Open(yamlFile)
+	yaml, err := ioutil.ReadAll(file)
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -19,18 +30,20 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-    - path: /urlshort
-      url: https://github.com/gophercises/urlshort
-    - path: /urlshort-final
-      url: https://github.com/gophercises/urlshort/tree/solution
-`
+	// if yaml == "" {
+	// 	yaml = `
+	// 	- path: /urlshort
+	// 	  url: https://github.com/gophercises/urlshort
+	// 	- path: /urlshort-final
+	// 	  url: https://github.com/gophercises/urlshort/tree/solution
+	// 	  `
+	// }
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Starting the server on :8083")
-	http.ListenAndServe(":8083", yamlHandler)
+	fmt.Println("Starting the server on :8084")
+	http.ListenAndServe(":8084", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
