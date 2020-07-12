e250ca93add7e169d57019ae1bfe5718a09d3043
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-07-12 10:47:17 +0200

    Quiz Game - Clean Code

--- quizgame/main.go
+++ quizgame/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"os"
 	"strings"
@@ -25,47 +24,17 @@ func init() {
 
 func main() {
 	flag.Parse()
-	var file []byte
-	var err error
-	file, err = ioutil.ReadFile(questionsFile)
 	reader := bufio.NewScanner(os.Stdin)
-
-	if err != nil {
-		fmt.Println("No se pudo cargar el archivo de preguntas")
-		return
-	}
-
 	success := 0
 	totalQuestions := 0
-	var shuffleData [][]string
+	csvFile := loadQuestionsFile()
+
 	if shuffle {
-		csvFile := csv.NewReader(strings.NewReader(string(file)))
-		file, err := os.Create(questionsFile)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		shuffleData, err = csvFile.ReadAll()
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(shuffleData), func(i, j int) {
-			shuffleData[i], shuffleData[j] = shuffleData[j], shuffleData[i]
-		})
-		data := csv.NewWriter(file)
-		data.WriteAll(shuffleData)
+		shuffleQuestions(csvFile)
+		csvFile = loadQuestionsFile()
 	}
-	csvFile := csv.NewReader(strings.NewReader(string(file)))
+
 	for {
-		data, err := csvFile.Read()
-		if err != nil {
-			break
-		}
-		if totalQuestions == 0 {
-			reader.Bytes()
-		}
-
-		question := string(data[0])
-		correctAnswer := string(data[1])
-
 		if totalQuestions == 0 {
 			fmt.Println("Presiona Enter para empezar")
 		} else {
@@ -73,28 +42,75 @@ func main() {
 		}
 		bufio.NewReader(os.Stdin).ReadRune()
 
+		question, correctAnswer, err := loadNextQuestion(csvFile)
+		if err != nil {
+			break
+		}
 		totalQuestions++
 		fmt.Print(question, " Respuesta: ")
+
 		timer := time.NewTimer(time.Duration(countDown) * time.Second)
 		go func() {
 			<-timer.C
-			fmt.Println("\nSe acabó del tiempo!!!\nGame Over")
+			fmt.Println("\nSe acabó el tiempo!!!\nGame Over")
 			gameOver(totalQuestions, success)
 			os.Exit(3)
 		}()
 
-		reader.Scan()
-		userAnswer := reader.Text()
-		if correctAnswer == strings.TrimSpace(strings.ToLower(userAnswer)) {
-			fmt.Println("Respuesta correcta")
-			success++
-		} else {
-			fmt.Println("Respuesta incorrecta")
-		}
+		checkAnswer(reader, correctAnswer, &success)
 		timer.Stop()
 	}
 	fmt.Println("\nJuego Completado! Enhorabuena!!!")
 	gameOver(totalQuestions, success)
+}
+
+func checkAnswer(reader *bufio.Scanner, correctAnswer string, success *int) {
+	reader.Scan()
+	userAnswer := reader.Text()
+	if correctAnswer == strings.TrimSpace(strings.ToLower(userAnswer)) {
+		fmt.Println("Respuesta correcta")
+		*success++
+	} else {
+		fmt.Println("Respuesta incorrecta")
+	}
+}
+
+func loadNextQuestion(csvFile *csv.Reader) (question, correctAnswer string, err error) {
+	data, err := csvFile.Read()
+	if err != nil {
+		return
+	}
+
+	question = string(data[0])
+	correctAnswer = string(data[1])
+
+	return
+
+}
+
+func loadQuestionsFile() *csv.Reader {
+	file, err := os.Open(questionsFile)
+
+	if err != nil {
+		fmt.Println("No se pudo cargar el archivo de preguntas")
+		os.Exit(1)
+	}
+	return csv.NewReader(file)
+}
+
+func shuffleQuestions(csvFile *csv.Reader) {
+	file, err := os.Create(questionsFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	shuffleData, err := csvFile.ReadAll()
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(shuffleData), func(i, j int) {
+		shuffleData[i], shuffleData[j] = shuffleData[j], shuffleData[i]
+	})
+	data := csv.NewWriter(file)
+	data.WriteAll(shuffleData)
 }
 
 func gameOver(totalQuestions, success int) {
